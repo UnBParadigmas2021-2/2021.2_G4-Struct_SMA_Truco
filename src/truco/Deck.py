a5d1fd75e21c6b96e630135fb55d77619d73f1b4
faco400 <vd.souza1999@gmail.com>
2022-04-17 10:07:23 -0300

    Fix deck to rule of truco paulista

--- src/truco/Deck.py
+++ src/truco/Deck.py
@@ -8,44 +8,57 @@ class Deck():
         self.create_deck_of_cards()
 
     def create_deck_of_cards(self):
-        ZAP = Cards(11, 1)
-        SETE_COPAS = Cards(10, 1)
-        ESPADILHA = Cards(9, 1)
-        SETE_OUROS = Cards(8, 1)
-        TRES = Cards(7, 4)
-        DOIS = Cards(6, 4)
-        AS = Cards(5, 3)
-        REI = Cards(4, 4)
-        VALETE = Cards(3, 4)
-        DAMA = Cards(2, 4)
+        # ZAP = Cards(11, 1)
+        # SETE_COPAS = Cards(10, 1)
+        # ESPADILHA = Cards(9, 1)
+        # SETE_OUROS = Cards(8, 1)
+        # TRES = Cards(7, 4)
+        # DOIS = Cards(6, 4)
+        # AS = Cards(5, 3)
+        # REI = Cards(4, 4)
+        # VALETE = Cards(3, 4)
+        # DAMA = Cards(2, 4)
 
-        self.cards.append(ZAP);
-        self.cards.append(DAMA);
-        self.cards.append(SETE_COPAS);
-        self.cards.append(VALETE);
-        self.cards.append(ESPADILHA);
-        self.cards.append(REI);
-        self.cards.append(SETE_OUROS);
-        self.cards.append(AS);
+        TRES = Cards(10, 4)
+        DOIS = Cards(9, 4)
+        AS = Cards(8, 4)
+        REI = Cards(7, 4)
+        VALETE = Cards(6, 4)
+        DAMA = Cards(5, 4)
+        SETE = Cards(4, 4)
+        SEIS = Cards(3, 4)
+        CINCO = Cards(2, 4)
+        QUATRO = Cards(1, 4)
+
+        # self.cards.append(ZAP);
+        # self.cards.append(SETE_COPAS);
+        # self.cards.append(ESPADILHA);
+        # self.cards.append(SETE_OUROS);
         self.cards.append(TRES);
         self.cards.append(DOIS);
+        self.cards.append(AS);
+        self.cards.append(REI);
+        self.cards.append(VALETE);
+        self.cards.append(DAMA);
+        self.cards.append(SETE);
+        self.cards.append(SEIS);
+        self.cards.append(CINCO);
+        self.cards.append(QUATRO);
 
     def shuffle_deck(self):
         random.shuffle(self.cards)
     
     def define_vira(self, deck):
-        # print("antes: "+ str(self.cards[0].quantity)) # Debbug do vira antes
-        self.vira = deck.remove_card() # o topo do deck vira manilha
-        # print("depois: "+ str(self.cards[0].quantity)) # Debbug do vira depois
+        print("antes: "+ str(self.cards[0].quantity)) # Debbug do vira antes
+        self.vira = deck.remove_card() # o topo do deck se torna vira
+        print("depois: "+ str(self.cards[0].quantity)) # Debbug do vira depois
 
     def remove_card(self):
         self.cards[0].quantity -= 1
         return self.cards[0] # retorna a carta no topo
 
     def define_manilha(self):
-        if self.vira.weight == 7:
-            return 11
-        elif self.vira.weight == 13:
+        if self.vira.weight == 10:
             return 1
         else:
             return self.vira.weight + 1
