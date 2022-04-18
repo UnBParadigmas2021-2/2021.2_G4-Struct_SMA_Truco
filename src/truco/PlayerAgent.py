019510e844b2499da2885bfc15503c4c37734465
Joao Pedro <jps12carvalho@gmail.com>
2022-04-18 19:45:50 -0300

    Adicionando logica para jogo otimizado dos agentes
    Co-authored-by: Victor Samuel <victor.samuelsantoss@gmail.com>
    Co-authored-by: Thiago Luiz <thiago.luiz20000@gmail.com>
    Co-authored-by: André Lucas <andre.lucas.lemos@hotmail.com>
    April 18, 2022

--- src/truco/PlayerAgent.py
+++ src/truco/PlayerAgent.py
@@ -14,6 +14,19 @@ class PlayerAgent(Agent):
       number_card = card.name
       self.mao.append([number_card, naipe, card.weight])
 
+  def compare_cards(self, card1, card2):
+    naipe = {'PAUS':1, 'COPAS':2, 'ESPADAS':3, 'OUROS':4}
+    if (card1[2] > card2[2]):
+      return -1
+    elif (card1[2] < card2[2]):
+      return 1
+    else:
+      id1 = naipe[card1[1]]
+      id2 = naipe[card2[1]]
+      if id1 < id2:
+        return -1
+      return 1
+
   def step(self):
     if not self.mao:
       self.set_hand()
@@ -21,9 +34,6 @@ class PlayerAgent(Agent):
       cards_list = [self.mao[i][0] + " de " + self.mao[i][1] for i in range(3)]
       print(", ".join(cards_list))
       return
-
-    print('------------------------------- lista de cartas da mao ---------------------------------------')
-    print(self.mao)
 
     max_card = 0
     min_card = 0
@@ -33,24 +43,20 @@ class PlayerAgent(Agent):
       min_card = min_card if self.mao[i][2] < self.mao[min_card][2] else i
       max_card = max_card if self.mao[i][2] > self.mao[max_card][2] else i
 
-    if not self.model.play:
+    if self.model.play == {}:
       played_card = min_card
-    else:
+    elif self.compare_cards(self.model.play['play'], self.mao[max_card]) == 1:
       played_card = max_card
-      
-
-
-    print('------------------------------- lista de cartas da mao ---------------------------------------')
-    play = {'pair': self.pair, 'play': self.mao[played_card]} #self.mao.pop()
-    print(f'play = {played_card}')
-
+    else:
+      played_card = min_card
+  
+    play = {'pair': self.pair, 'play': self.mao[played_card]} 
 
     self.mao.pop(played_card)
 
-
-
-
-    #print(self.name, "joga", play[0], "de", play[1], play[2])
-
-    print(f'cartas da mesa = {self.model.play}')
-    self.model.play = play
+    print(f'Maior carta na mesa = {self.model.play}')
+    print(f'Carta a ser jogada = {play}')
+    if self.model.play == {} or self.compare_cards(self.model.play['play'], play['play']) == 1:
+      self.model.play = play
+    print(f'Maior carta na mesa dps de ser atualizada = {self.model.play}')
+    
