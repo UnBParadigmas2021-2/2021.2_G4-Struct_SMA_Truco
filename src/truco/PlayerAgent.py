dd3574137eb1783acf98f5d72be0b5d7030f6e28
RuanMoura <ruanmoura13@outlook.com>
2022-04-18 14:19:27 -0300

    Add rules to agent play a unique round

--- src/truco/PlayerAgent.py
+++ src/truco/PlayerAgent.py
@@ -1,27 +1,26 @@
 from mesa import Agent
 
 class PlayerAgent(Agent):
-  def __init__(self, unique_id, model, deck, name):
+  def __init__(self, unique_id, model, name, pair):
     super().__init__(unique_id, model)
     self.name = name
-    self.deck = deck
+    self.pair = pair
     self.mao = []
 
   def set_hand(self):
     for i in range(3):
-      card = self.deck.remove_card()
+      card = self.model.deck.remove_card()
       naipe = card.return_random_naipe()
       number_card = card.return_number_card()
-      self.mao.append([number_card, naipe])
-
-    print("mao :") # Imprimindo mao para debug
-    for i in range(3):
-      print(self.mao[i])
-
+      self.mao.append([number_card, naipe, card.weight])
 
   def step(self):
     if not self.mao:
-      print("Ola, eu sou o agente "+ self.name) 
       self.set_hand()
+      print("Ola, eu sou o agente", self.name, "e essas são minhas cartas:")
+      for i in range(3):
+        print(self.mao[i][0], "de", self.mao[i][1])
       return
-    print(self.name, "jogando")
+    play = self.mao.pop()
+    print(self.name, "joga", play[0], "de", play[1])
+    self.model.play.append({'pair': self.pair, 'play': play})
--- src/truco/PlayerModel.py
+++ src/truco/PlayerModel.py
@@ -9,22 +9,49 @@ class PlayerModel(Model):
     # self.nome = nome # Setando o nome do player
     # self.mao = [] # Setando a mao do player
     self.agents = []
-
     self.schedule = BaseScheduler(self) # instanciando um scheduler para ordenar os steps dos agentes
     self.num_agents = N
+    self.play = []
+    self.pairs_points = [0, 0]
+    self.partial_pair_points = [0, 0]
+    self.deck = deck
 
     for i in range(self.num_agents):
       # player_name = str(input("Nome do Jogador " + str(i+1) + " : "))
       names = ['a','b','c','d']
       player_name = names[i]
-      a = PlayerAgent(i, self, deck, player_name) # Criando o PlayerAgent
+      a = PlayerAgent(i, self, player_name, i%2) # Criando o PlayerAgent
       self.schedule.add(a) # adicionando o agente ao schedule
       self.agents.append(a)
 
   def step(self): # Usado para debugar o step dos agentes. No momento imprime no console o id do agente
     self.schedule.step()
+    print('--------------------------')
     for _ in range(3):
       self.schedule = BaseScheduler(self)
       for j in self.agents:
         self.schedule.add(j)
       self.schedule.step()
+      self.step_point()
+      self.play = []
+      print('--------------------------')
+      if self.partial_pair_points[0] == 2:
+        print("Dupla 1 ganhou!")
+        break
+      if self.partial_pair_points[1] == 2:
+        print("Dupla 2 ganhou!")
+        break
+
+
+  def step_point(self):
+    pair_play = [0,0]
+    for play in self.play:
+      pair_play[play['pair']] = max(pair_play[play['pair']], play['play'][2])
+    if pair_play[0] > pair_play[1]:
+      print("Ponto da dupla 1")
+      self.partial_pair_points[0] += 1
+    elif pair_play[1] > pair_play[0]:
+      print("Ponto da dupla 2")
+      self.partial_pair_points[1] += 1
+    else:
+      print("Empate")
